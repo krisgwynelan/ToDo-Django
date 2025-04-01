7435cb73380dacf88d0a52089c1fcacd91622740
krisgwynelan <elan.krisgwyn08@gmail.com>
2025-04-01 22:26:01 +0800

    ToDo Django

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ToDo-Django/', // Set this to your repo name
+  base: '/ToDo-Django/', 
 });
